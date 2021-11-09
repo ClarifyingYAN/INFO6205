e5bdd7a3d14450935b508ead10587d5d591714b0
ChengYan@Mac <hu_yancheng@outlook.com>
2021-11-08 22:43:59 -0500

    Assignment5 parallel sort (coding part)

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -4,10 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 /**
@@ -17,41 +14,78 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
-        processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+//        processArgs(args);
+        // set thread number to run
+        int base=1;
+        List<Integer> threadCounts = new ArrayList<>();
+        for (int i = 0; i < 4; i++) // 2, 4, 8, 16
+            threadCounts.add(base*=2);
+
         Random random = new Random();
-        int[] array = new int[2000000];
-        ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-            long time;
-            long startTime = System.currentTimeMillis();
-            for (int t = 0; t < 10; t++) {
-                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+
+        for (Integer threadCount : threadCounts) {
+            // TODO: 1. set the number of available threads
+            ParSort.setMyPool(new ForkJoinPool(threadCount));
+            System.out.println("Degree of parallelism: " + ParSort.getMyPool().getParallelism());
+            List<Long> timeList = new ArrayList<>(); // for same thread but different cutoffs
+            List<Integer> cutoffs = new ArrayList<>();
+            List<Integer> arraySizes = new ArrayList<>();
+
+            for (int arraySize = 2000000; arraySize <= 10000000; arraySize+=1000000) {
+                int halfArraySize = arraySize/2;
+                int[] array = new int[arraySize];
+
+                for (int cutoff = 1000; cutoff <= halfArraySize; cutoff+=cutoff) {
+                    // TODO: 2. set cutoff, relative to size of the array
+                    ParSort.setCutoff(cutoff);
+                    long time=0L, startTime, endTime;
+                    for (int t = 0; t < 10; t++) {
+                        for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                        startTime = System.currentTimeMillis();
+                        ParSort.sort(array, 0, array.length);
+                        endTime = System.currentTimeMillis();
+                        time += endTime-startTime;
+                    }
+
+                    timeList.add(time/10);
+                    arraySizes.add(arraySize);
+                    cutoffs.add(cutoff);
+
+                    StringBuilder sb = new StringBuilder();
+                    sb.append("arraySize: ").append(arraySize).append(" ,");
+                    sb.append("cutoff: ").append(cutoff).append(" ,");
+                    sb.append("time/10: ").append(time/10).append(" ms");
+                    System.out.println(sb.toString());
+
+                    System.out.println("cutoff：" + (ParSort.getCutoff()) + "\t\t10times Time:" + time + "ms");
+                }
             }
-            long endTime = System.currentTimeMillis();
-            time = (endTime - startTime);
-            timeList.add(time);
 
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
+            writeToFile(threadCount, timeList, cutoffs, arraySizes);
         }
+    }
+
+    private static void writeToFile(int threadCount, List<Long> timeList, List<Integer> cutoffs, List<Integer> arraySizes) {
+        System.out.println("Starting to write to file");
+        String path = "./src/";
+        String fileName = path + threadCount + "threads-result.csv";
+        System.out.println("Write to: " + fileName);
         try {
-            FileOutputStream fis = new FileOutputStream("./src/result.csv");
+            FileOutputStream fis = new FileOutputStream(fileName);
             OutputStreamWriter isr = new OutputStreamWriter(fis);
             BufferedWriter bw = new BufferedWriter(isr);
-            int j = 0;
-            for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
-                j++;
+            System.out.println(timeList.size());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < timeList.size(); i++) {
+                sb.append(arraySizes.get(i)).append(",");
+                sb.append(cutoffs.get(i)).append(",");
+                sb.append(timeList.get(i)).append("\n");
+                String content = sb.toString();
                 bw.write(content);
                 bw.flush();
+                sb.setLength(0);
             }
             bw.close();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -85,5 +119,10 @@ public class Main {
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private static final Map<String, Integer> configuration = new HashMap<>();
 
-
+    private static boolean isSorted(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i-1]) return false;
+        }
+        return true;
+    }
 }
--- src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
@@ -2,43 +2,40 @@ package edu.neu.coe.info6205.sort.par;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * This code has been fleshed out by Ziyao Qiao. Thanks very much.
  * TODO tidy it up a bit.
+ * Parallel Sort
  */
 class ParSort {
 
-    public static int cutoff = 1000;
+    private static int cutoff = 1000;
 
+    // the number of available threads, should be 2^n
+    // use common pool as default, which use core number of your pc as thread level
+    private static ForkJoinPool myPool = ForkJoinPool.commonPool();
+
+    /**
+     *
+     * @param array
+     * @param from index of the first key
+     * @param to length of the part array to be sorted ( = last index + 1)
+     */
     public static void sort(int[] array, int from, int to) {
-        if (to - from < cutoff) Arrays.sort(array, from, to);
+        if (to - from < cutoff) // use system sort when number of elements is less than cutoff
+            Arrays.sort(array, from, to);
         else {
             // FIXME next few lines should be removed from public repo.
-            CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2); // TO IMPLEMENT
-            CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to); // TO IMPLEMENT
-            CompletableFuture<int[]> parsort = parsort1.thenCombine(parsort2, (xs1, xs2) -> {
-                int[] result = new int[xs1.length + xs2.length];
-                // TO IMPLEMENT
-                int i = 0;
-                int j = 0;
-                for (int k = 0; k < result.length; k++) {
-                    if (i >= xs1.length) {
-                        result[k] = xs2[j++];
-                    } else if (j >= xs2.length) {
-                        result[k] = xs1[i++];
-                    } else if (xs2[j] < xs1[i]) {
-                        result[k] = xs2[j++];
-                    } else {
-                        result[k] = xs1[i++];
-                    }
-                }
-                return result;
-            });
-
-            parsort.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
-//            System.out.println("# threads: "+ ForkJoinPool.commonPool().getRunningThreadCount());
-            parsort.join();
+            int mid = from + (to - from) / 2;
+            // sort the first part and the second part
+            CompletableFuture<int[]> firstPart = parsort(array, from, mid); // TO IMPLEMENT
+            CompletableFuture<int[]> secondPart = parsort(array, mid, to); // TO IMPLEMENT
+            // merge two parts
+            CompletableFuture<int[]> cf = firstPart.thenCombine(secondPart, ParSort::merge);
+            cf.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
+            cf.join();
         }
     }
 
@@ -51,6 +48,41 @@ class ParSort {
                     sort(result, 0, to - from);
                     return result;
                 }
-        );
+        , myPool);
+    }
+
+    private static int[] merge(int[] xs1, int[] xs2) {
+        int[] result = new int[xs1.length + xs2.length];
+        int i=0, j=0;
+        for (int k = 0; k < result.length; k++) {
+            if (i >= xs1.length) result[k] = xs2[j++];
+            else if (j >= xs2.length) result[k] = xs1[i++];
+            else if (xs2[j] < xs1[i]) result[k] = xs2[j++];
+            else result[k] = xs1[i++];
+        }
+
+        return result;
+    }
+
+    public static void setCutoff(int cutoff) {
+        ParSort.cutoff = cutoff;
+    }
+
+    public static void setMyPool(ForkJoinPool myPool) {
+        if (ParSort.myPool!=null)
+            shutdownPool();
+        ParSort.myPool = myPool;
+    }
+
+    public static ForkJoinPool getMyPool() {
+        return myPool;
+    }
+
+    public static int getCutoff() {
+        return cutoff;
+    }
+
+    public static void shutdownPool() {
+        myPool.shutdown();
     }
 }
