35c6239d3fda15607a71ca70513107bccf523ab6
ChengYan@Mac <hu_yancheng@outlook.com>
2021-11-10 12:39:03 -0500

    Assignment 5

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -16,65 +16,58 @@ public class Main {
     public static void main(String[] args) {
 //        processArgs(args);
         // set thread number to run
-        int base=1;
-        List<Integer> threadCounts = new ArrayList<>();
-        for (int i = 0; i < 4; i++) // 2, 4, 8, 16
-            threadCounts.add(base*=2);
+        int threadCount = 4;
+        // set array size
+        int arraySize = 6000000;
 
         Random random = new Random();
 
-        for (Integer threadCount : threadCounts) {
-            // TODO: 1. set the number of available threads
-            ParSort.setMyPool(new ForkJoinPool(threadCount));
-            System.out.println("Degree of parallelism: " + ParSort.getMyPool().getParallelism());
-            List<Long> timeList = new ArrayList<>(); // for same thread but different cutoffs
-            List<Integer> cutoffs = new ArrayList<>();
-            List<Integer> arraySizes = new ArrayList<>();
+        // TODO: 1. set the number of available threads
+        ParSort.setMyPool(new ForkJoinPool(threadCount));
+        System.out.println("Degree of parallelism: " + ParSort.getMyPool().getParallelism());
+        List<Long> timeList = new ArrayList<>(); // for same thread but different cutoffs
+        List<Integer> cutoffs = new ArrayList<>();
+        List<Integer> arraySizes = new ArrayList<>();
 
-            for (int arraySize = 2000000; arraySize <= 10000000; arraySize+=1000000) {
-                int halfArraySize = arraySize/2;
-                int[] array = new int[arraySize];
+        int[] array = new int[arraySize];
 
-                for (int cutoff = 1000; cutoff <= halfArraySize; cutoff+=cutoff) {
-                    // TODO: 2. set cutoff, relative to size of the array
-                    ParSort.setCutoff(cutoff);
-                    long time=0L, startTime, endTime;
-                    for (int t = 0; t < 10; t++) {
-                        for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                        startTime = System.currentTimeMillis();
-                        ParSort.sort(array, 0, array.length);
-                        endTime = System.currentTimeMillis();
-                        time += endTime-startTime;
-                    }
-
-                    timeList.add(time/10);
-                    arraySizes.add(arraySize);
-                    cutoffs.add(cutoff);
-
-                    StringBuilder sb = new StringBuilder();
-                    sb.append("arraySize: ").append(arraySize).append(" ,");
-                    sb.append("cutoff: ").append(cutoff).append(" ,");
-                    sb.append("time/10: ").append(time/10).append(" ms");
-                    System.out.println(sb.toString());
-
-                    System.out.println("cutoff：" + (ParSort.getCutoff()) + "\t\t10times Time:" + time + "ms");
-                }
+        for (int cutoff = 23437; cutoff < arraySize; cutoff+=cutoff) {
+            // TODO: 2. set cutoff, relative to size of the array
+            ParSort.setCutoff(cutoff);
+            long time=0L, startTime, endTime;
+            for (int t = 0; t < 10; t++) {
+                for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+                startTime = System.currentTimeMillis();
+                ParSort.sort(array, 0, array.length);
+                endTime = System.currentTimeMillis();
+                time += endTime-startTime;
             }
 
-            writeToFile(threadCount, timeList, cutoffs, arraySizes);
+            timeList.add(time/10);
+            arraySizes.add(arraySize);
+            cutoffs.add(cutoff);
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("arraySize: ").append(arraySize).append(" ,");
+            sb.append("cutoff: ").append(cutoff).append(" ,");
+            sb.append("time/10: ").append(time/10).append(" ms");
+            System.out.println(sb.toString());
+
+            System.out.println("cutoff：" + (ParSort.getCutoff()) + "\t\t10times Time:" + time + "ms");
         }
+        writeToFile(threadCount, arraySize, timeList, cutoffs, arraySizes);
     }
 
-    private static void writeToFile(int threadCount, List<Long> timeList, List<Integer> cutoffs, List<Integer> arraySizes) {
+    private static void writeToFile(int threadCount, int arraySize, List<Long> timeList, List<Integer> cutoffs, List<Integer> arraySizes) {
         System.out.println("Starting to write to file");
         String path = "./src/";
-        String fileName = path + threadCount + "threads-result.csv";
+        String fileName = path + arraySize + "arraySize" + threadCount + "threads-result.csv";
         System.out.println("Write to: " + fileName);
         try {
             FileOutputStream fis = new FileOutputStream(fileName);
             OutputStreamWriter isr = new OutputStreamWriter(fis);
             BufferedWriter bw = new BufferedWriter(isr);
-            System.out.println(timeList.size());
+//            System.out.println(timeList.size());
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < timeList.size(); i++) {
                 sb.append(arraySizes.get(i)).append(",");
